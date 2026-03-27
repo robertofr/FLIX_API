a6d2d143315b84d39f2640b82930f756faa28226
Roberto F. Rodrigues <robertofr_@hotmail.com>
2026-03-26 22:28:36 -0300

    Funções de validação com serializers

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,7 +1,30 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movies
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieModelSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movies
         fields = '__all__'
+
+    def get_rate(self, obj):
+       rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+       
+       if rate: 
+           return round(rate, 1)
+
+       return None    
+
+    def validate_release_date(self, value):
+        if value.year < 1950:
+            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1950.')
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 255:
+            raise serializers.ValidationError('O resumo não deve ser maior que 255 caracteres.')
+        return value
+    
+    
--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,12 @@
 from rest_framework import generics
 from movies.models import Movies
-from movies.serializers import MovieSerializer
+from movies.serializers import MovieModelSerializer
 
 
 class MovieCreateListView(generics.ListCreateAPIView):
     queryset = Movies.objects.all()
-    serializer_class = MovieSerializer
+    serializer_class = MovieModelSerializer
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movies.objects.all()
-    serializer_class = MovieSerializer
+    serializer_class = MovieModelSerializer
