9041a6c55982261a10e5c1de98b8f9ddff985363
Roberto F. Rodrigues <robertofr_@hotmail.com>
2026-04-09 21:48:00 -0300

    Configuração do arquivo de permissões

--- movies/views.py
+++ movies/views.py
@@ -2,14 +2,15 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from movies.models import Movies
 from movies.serializers import MovieModelSerializer
+from app.permissions import GlobalDefaultPermission
 
 
 class MovieCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movies.objects.all()
     serializer_class = MovieModelSerializer
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movies.objects.all()
     serializer_class = MovieModelSerializer
